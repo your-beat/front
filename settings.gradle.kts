0ea830e82b46aece50a03bcab47d3b804480903d
Lee-JeongSoo <chaola3503@gmail.com>
2023-11-23 18:54:45 +0900

    [feat] 프로젝트 생성

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "your_beat_front"
+include(":app")
+ 
