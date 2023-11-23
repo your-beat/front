177f7657fce3308c22a79d4e9db3cc6e2d17beba
Lee-JeongSoo <chaola3503@gmail.com>
2023-11-23 19:09:10 +0900

    [feat] home view activity 생성

---
+++ app/src/main/java/com/example/your_beat_front/HomeViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.your_beat_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_view)
+    }
+}
