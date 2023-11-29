15d47b39b0b459cde45750b0bb3c366f7bf1ddc5
Lee-JeongSoo <chaola3503@gmail.com>
2023-11-29 15:50:21 +0900

    [feat] main activity 뷰 생성 - home view-> main으로 수정 - 네비를 통해 전환되는 화면들은 fragment로 구현 예정 - text style과 font 추가 - 최소 sdk 24->28로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.your_beat_front"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -44,4 +44,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.android.material:material:1.3.0")
 }
