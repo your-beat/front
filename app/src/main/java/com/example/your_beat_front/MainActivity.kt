91258ddf742a5ff686a269f084f899424c382f13
Lee-JeongSoo <chaola3503@gmail.com>
2023-12-01 15:44:41 +0900

    [add] 내 기기 카드 추가 - drawable 에 카드 , 그래디언트 추가 - item_device_off_card.xml/item_device_on_card.xml 추가

--- app/src/main/java/com/example/your_beat_front/MainActivity.kt
+++ app/src/main/java/com/example/your_beat_front/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.your_beat_front
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        initLayout()
+//    }
+//
+//    private fun initLayout() {
+//        val isMyPageRequested = intent.getStringExtra(MY_PAGE_FRAGMENT) == MY_PAGE_FRAGMENT
+//        val isBakeryListRequested =
+//            intent.getStringExtra(BAKERY_LIST_FRAGMENT) == BAKERY_LIST_FRAGMENT
+//
+//        val initialFragment = if (isMyPageRequested) {
+//            navigateTo<MyPageFragment>()
+//            R.id.menu_my_page
+//        } else if (isBakeryListRequested) {
+//            navigateTo<BakeryListFragment>()
+//            R.id.menu_bakery_list
+//        } else {
+//            navigateTo<HomeFragment>()
+//            R.id.menu_home
+//        }
+//
+//        binding.bnvHome.setOnItemSelectedListener { menu ->
+//            when (menu.itemId) {
+//                R.id.menu_home -> navigateTo<HomeFragment>()
+//                R.id.menu_bakery_list -> navigateTo<BakeryListFragment>()
+//                R.id.menu_my_page -> navigateTo<MyPageFragment>()
+//            }
+//            true
+//        }
+//
+//        binding.bnvHome.apply {
+//            menu.findItem(initialFragment)?.isChecked = true
+//            itemIconTintList = null
+//        }
+//    }
+//
+//    private inline fun <reified T : Fragment> navigateTo() {
+//        supportFragmentManager.commit {
+//            replace<T>(R.id.fcv_home_container, T::class.java.canonicalName)
+//        }
+//    }
+//
+//    companion object {
+//        const val MY_PAGE_FRAGMENT = "MyPageFragment"
+//        const val BAKERY_LIST_FRAGMENT = "BakeryListFragment"
+//    }
 }
